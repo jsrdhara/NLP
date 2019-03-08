c42be76b9453e9e7344c0d5b14970c93d2fadcfa
Jayarama Kapil Sridhara <46280713+jsrdhara@users.noreply.github.com>
2019-03-07 21:21:05 -0600

    Update BaseLineLexicalizedTagger.py

--- BaseLineLexicalizedTagger.py
+++ BaseLineLexicalizedTagger.py
@@ -7,7 +7,6 @@ from collections import Counter
 from collections import defaultdict
 from collections import OrderedDict
 
-# Cleans entire brown corpus and returns an output file with just word and their tags.
 def CleanFile(In,Out):
     Regex = re.compile(r"([A-Z$]+ [\w]+)")
     with open(In , 'r+') as f:
@@ -22,7 +21,6 @@ def CleanFile(In,Out):
     with open(Out,mode ='a') as outfile:
         outfile.write(sentence)
 
-# This function returns a Hash of Hash Dictionary with each word, the possible tag of the word and their counts--> {word:{tag1:count1,tag2:count2..}}
 def HashOfHash():
     global hash_dict
     with open(TrainingOutputfile, 'r') as f:
@@ -35,7 +33,6 @@ def HashOfHash():
     hash_dict = {k: dict(Counter(v)) for k, v in word_dict.items()}  
 
 
-# Using hash of hashes function to train a baseline lexicalized statistical tagger on the entire BROWN corpus.
 def Statistical_tagger():
     global trained_tagger
     trained_tagger = {}
@@ -43,8 +40,6 @@ def Statistical_tagger():
         frequent_tag = sorted(taglist.items(), key=lambda x: x[1],reverse=True)[0][0]
         trained_tagger[word] = frequent_tag
 
-
-# Using the baseline lexicalized statistical tagger to tag all the words in the SnapshotBROWN file.
 def TagSnapshot():
     global Evaluated_tags
     global Original_tags
@@ -64,7 +59,6 @@ def TagSnapshot():
                     Original_tags.append(data[i-1])
                     Evaluated_tags.append(trained_tagger[data[i]])  
 
-# This funciton returns the evaluation parameters of the baseline lexicalised tagger such as accuracy,error and number of words that are not tagged
 def EvaluateTagger():
     count = 0
     for i in range(0,len(Evaluated_tags)):
@@ -79,7 +73,6 @@ def EvaluateTagger():
     print('The number of words that are not tagged    :    {}  '.format(len(Unknown_Words)))
     
 
-# This function adds few rules to handle unknown words for the base line lexicalised tagger
 def Tag_New_Words(word):
 
     modals = ["can", "could", "may", "might", "will", "would", "shall", "should", "must"]
@@ -144,7 +137,6 @@ def Tag_New_Words(word):
         return "<NONE>"
     
 
-# This function cleans the news article that is used for testing of lexicalised tagger with new rules added
 def CleanNewsArticle(In):
     global text
     with io.open(In,'r',encoding = 'utf-8') as f:
@@ -153,7 +145,7 @@ def CleanNewsArticle(In):
         text = [x.encode('UTF8') for x in text]
         
 
-# This function Tags all the words,punctutations and others in the Newspaper article
+
 def TagNewsPaper():
     global TagContent,Count_Total_Words, Count_Known_Words, Count_New_Words,Count_New_Words_Tagged, Count_New_Words_NotTagged
     Count_Total_Words = len(text)
@@ -171,8 +163,7 @@ def TagNewsPaper():
             else:
                 Count_New_Words_Tagged +=1
 
-
-# This function displays the words and tags of the Newspaper file by using the trained tagger with added rules               
+        
 def DisplayNewspaperTags():
     print('Tagging Words in Newspaper using New Tagger ')
     for key,value in TagContent.items():
@@ -185,8 +176,7 @@ def DisplayNewspaperTags():
         elif Tag_New_Words(key) == "<NONE>":
             print("Word (UnKnown-Not-Tagged)      : %6s      %s"% (value, key))
 
-
-# This fucntion prints the Performance metrics of the New Tagger with added rules.            
+      
 def PerformanceNewTagger():
     Percentile_Known_Words = float(Count_Known_Words*100)/Count_Total_Words
     Percentile_New_Words = float(Count_New_Words*100)/Count_Total_Words
@@ -205,8 +195,7 @@ def PerformanceNewTagger():
 
     print('Words Could Not Tag (percentile in new words)     : {} ({}%)'.format(Count_New_Words_NotTagged,Percentile_New_Words_Not_Tagged))
 
-
-# Main function to run all the aove defined functions    
+ 
 def main():
     
     global TrainingInputfile,TrainingOutputfile,TestInputFile,TestOutputFile,InputNewspaper
